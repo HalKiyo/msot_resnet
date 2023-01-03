2db0b70c4b7be2153b382695380bb0e600853414
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2023-01-03 22:38:38 +0900

    view.py added for true false viasual

---
+++ class/one/view.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def draw_val(val_pred, val_label):
+    fig = plt.figure()
+    ax = plt.subplot()
+
+    val_list = [1, 2, 3, 4, 5]
+    width, linewidth, align = 0.4, 0, 'edge'
+
+    # count
+    val_true = []
+    val_false = []
+    for i, j in zip(val_pred, val_label):
+        if np.argmax(i) == np.argmax(j):
+            val_true.append(val_list[np.argmax(i)])
+        else:
+            val_false.append(val_list[np.argmax(i)])
+
+    # true
+    val_tcount = [val_true.count(j) for j in val_list]
+    ax.bar(val_list, val_tcount,
+           color='darkslategray', width=width, linewidth=linewidth, align=align)
+
+    # false
+    val_fcount = [val_false.count(j) for j in val_list]
+    ax.bar(val_list, val_fcount,
+           color='orange', bottom=val_tcount, width=width, linewidth=linewidth, align=align, alpha=.8)
+
+    plt.show()
+
+
+
